fd9383b01e619bea6549105af970b2c21c99180e
Darkness Hidden(TEA) <kmd36rk@gmail.com>
2021-05-05 18:10:30 +0700

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -4,7 +4,7 @@ cd $ANDROID_ROOT
 
 source build/envsetup.sh
 export USE_CCACHE=1
-breakfast $DEVICE
+breakfast lineage_land-eng
 mka recoveryimage
 md5sum $ANDROID_ROOT/out/target/product/land/recovery.img
 
