efe9fa29e433d4ec407fbcc554842285d7a21929
Darkness Hidden(TEA) <kmd36rk@gmail.com>
2021-10-25 16:29:57 +0700

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -4,7 +4,14 @@ cd $ANDROID_ROOT
 
 source build/envsetup.sh
 export USE_CCACHE=1
-breakfast lineage_land-eng
+breakfast $DEVICE
+mka mkbootimg
+mka hybris-boot
+mka halium-boot
 mka recoveryimage
-md5sum $ANDROID_ROOT/out/target/product/land/recovery.img
+mka systemimage 
 
+echo "md5sum halium-boot.img and system.img"
+md5sum $ANDROID_ROOT/out/target/product/land/halium-boot.img
+md5sum $ANDROID_ROOT/out/target/product/land/recovery.img
+md5sum $ANDROID_ROOT/out/target/product/land/system.img
