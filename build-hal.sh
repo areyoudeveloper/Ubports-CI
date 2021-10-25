d9d80ad2f3b53abff2e57fc46622116087c352c6
Darkness Hidden(TEA) <kmd36rk@gmail.com>
2021-10-25 17:25:34 +0700

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -12,6 +12,6 @@ mka recoveryimage
 mka systemimage 
 
 echo "md5sum halium-boot.img and system.img"
-md5sum $ANDROID_ROOT/out/target/product/land/halium-boot.img
-md5sum $ANDROID_ROOT/out/target/product/land/recovery.img
-md5sum $ANDROID_ROOT/out/target/product/land/system.img
+md5sum $ANDROID_ROOT/out/target/product/lantoni/halium-boot.img
+md5sum $ANDROID_ROOT/out/target/product/lantoni/recovery.img
+md5sum $ANDROID_ROOT/out/target/product/lantoni/system.img
